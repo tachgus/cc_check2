25ed758340b9d1260768682c98d9fe246e76b6c8
Tim den Ouden <tachgus@gmail.com>
2016-12-06 16:48:13 +0100

    Creditcard length should be minimal 12 positions.

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,6 +1,7 @@
 class Creditcard < ApplicationRecord
+  # this include is neede for creditcard validation
   include ActiveModel::Validations
 
-  validates :card_number, :presence => true, length: {minimum: 10, message: ' moet minstens 10 tekens zijn.'}
+  validates :card_number, :presence => true, length: {minimum: 12, message: ' moet minstens 12 tekens zijn.'}
   validates :card_number, credit_card_number: true
 end
